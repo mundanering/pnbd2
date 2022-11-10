f60e46d6fab003a7a70ac09743295847c9161333
Mik3l24 <michalczus@gmail.com>
2022-11-10 20:57:37 +0100

    Dodanie testowego template'u

--- imageboard/views.py
+++ imageboard/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def index(request):
+
+    context = {
+        "test": "hash",
+
+               }  # tutaj dajemy zmienne z którego template może korzystać
+    return render(request, "imageboard/index.html", context)
