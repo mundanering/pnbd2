795f87ccf5e17552eea25b4e6f712d5433d04b78
mundanering <mundanering@gmail.com>
2022-11-17 23:37:39 +0100

    Changed success message after registering

--- user/views.py
+++ user/views.py
@@ -9,7 +9,7 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get("username")
-            messages.success(request, f"Konto {username} zostało pomyślnie założone!")
+            messages.success(request, f"Konto o nazwie {username} zostało pomyślnie założone!")
             return redirect("login")
     else:
         form = UserCreationForm()
