3448116421813e65bd1b46f13ebee8501a0b2a1f
Mik3l24 <michalczus@gmail.com>
2022-11-17 13:18:24 +0100

    Added the Post model Refactored all strings to use "" instead of '' Added another example to test.html

--- imageboard/models.py
+++ imageboard/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Post(models.Model):
     contents = models.TextField(max_length=200)
     image_link = models.URLField(max_length=50)
     date_posted = models.DateTimeField()
-
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
