26c44aa70222cd4051d2b93868a17d055df40718
mundanering <mundanering@gmail.com>
2022-11-24 19:37:10 +0100

    Added icons for posts, like/dislike buttons, missing <form> tag for search input + did some other minor frontend changes

--- imageboard/views.py
+++ imageboard/views.py
@@ -6,15 +6,18 @@ from .models import Post
 
 
 def test(request):
-    """Render the template."""
+    """Render the test template."""
     # Tutaj dajemy zmienne, z którego template może korzystać
     content = {
         "test": "hash",
         "image_link": "https://asset-eu.unileversolutions.com/content/dam/unilever/lipton_international/poland/pack_design/5997264159313-1588506-png.png.ulenscale.490x490.png"
-        }
+    }
     return render(request, "imageboard/test.html", content)
+
+
 def home(request, year=datetime.now().year, month=datetime.now().strftime('%B')):
-    name = "kill me"
+    """Render the home page."""
+    name = "kill all biggers"
     month = month.capitalize()
     month_number = list(calendar.month_name).index(month)
     month_number = int(month_number)
@@ -29,23 +32,20 @@ def home(request, year=datetime.now().year, month=datetime.now().strftime('%B'))
 
     # Query the posts Model For Dates
     post_list = Post.objects.filter(
-        date_posted__year = year,
-        date_posted__month = month_number
-        )
+        date_posted__year=year,
+        date_posted__month=month_number
+    )
 
     # Get current time
     time = now.strftime('%I:%M %p')
     return render(request,
-        'imageboard/home.html', {
-        "name": name,
-        "year": year,
-        "month": month,
-        "month_number": month_number,
-        "cal": cal,
-        "current_year": current_year,
-        "time":time,
-        "post_list": post_list,
-            })
-
-
-
+                  'imageboard/home.html', {
+                      "name": name,
+                      "year": year,
+                      "month": month,
+                      "month_number": month_number,
+                      "cal": cal,
+                      "current_year": current_year,
+                      "time": time,
+                      "post_list": post_list,
+                  })
